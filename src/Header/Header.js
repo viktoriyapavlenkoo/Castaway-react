914ee13db9ec798e4ba633d6da0a79dae897e52c
Viktoriya <viktoriyapavlenkoo418@gmail.com>
2023-11-29 18:29:09 +0200

    Added burger menu.

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -5,22 +5,13 @@ import '../Header/Header.css';
 function Header() {
     const headerNavRef = React.createRef();
     const burgerRef = React.createRef();
-    const closeRef = React.createRef();
 
-    function openMenu() {
-        headerNavRef.current.style.display = "flex";
-        headerNavRef.current.classList.toggle('nav__mobile')
-        // burgerRef.current.style.display = "none";
-        closeRef.current.style.display = "block";
+    function burgerMenu() {
+        burgerRef.current.classList.toggle('active');
+        headerNavRef.current.classList.toggle('active')
     }
 
-    function closeMenu() {
-        headerNavRef.current.classList.toggle('nav__mobile')
-        headerNavRef.current.style.display = "none";
-        // burgerRef.current.style.display = "block";
-        closeRef.current.style.display = "none";
-    }
-
+  
 
     return (
         <header className="header">
@@ -31,13 +22,9 @@ function Header() {
                 <div className="header__nav" ref={headerNavRef}>
                     <Nav/>
                 </div>
-                <div className='burger' ref={burgerRef} onClick={openMenu}>
-                    <div className='burger__line'></div>
+                <div className='burger' ref={burgerRef} onClick={burgerMenu}>
+                     <span></span>
                 </div>
-                <div className="close-menu" ref={closeRef} onClick={closeMenu}>
-                    <div className='close-menu__line'></div>
-                </div>
-                
             </div>
         </header>
     )
